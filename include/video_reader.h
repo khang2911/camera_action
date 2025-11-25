6b038f4588e1cfa1e5cb2c4447b5f665bb909aad
khang2911 <khang.2911@gmail.com>
2025-11-25 17:05:49 +0700

    update

--- include/video_reader.h
+++ include/video_reader.h
@@ -2,18 +2,8 @@
 #define VIDEO_READER_H
 
 #include <opencv2/opencv.hpp>
+#include <memory>
 #include <string>
-#include <vector>
-
-extern "C" {
-#include <libavformat/avformat.h>
-#include <libavcodec/avcodec.h>
-#include <libavutil/imgutils.h>
-#include <libavutil/avutil.h>
-#include <libavutil/opt.h>
-#include <libavutil/hwcontext.h>
-#include <libswscale/swscale.h>
-}
 
 #include "video_clip.h"
 
@@ -35,29 +25,8 @@ public:
 private:
     void initializeMetadata();
     double computeFps(double reported_duration) const;
-    bool initialize(const VideoClip* clip);
-    bool openInput();
-    bool setupDecoder();
-    bool initHardwareDecoder(const AVCodec* decoder);
-    static AVPixelFormat getHWFormat(AVCodecContext* ctx, const AVPixelFormat* pix_fmts);
-    bool sendNextPacket();
-    bool receiveFrame(cv::Mat& frame);
-    bool convertFrameToMat(AVFrame* frame, cv::Mat& out);
-    void cleanup();
-    static void ensureFFmpegInitialized();
     
-    AVFormatContext* fmt_ctx_;
-    AVCodecContext* codec_ctx_;
-    AVStream* video_stream_;
-    AVPacket* packet_;
-    AVFrame* frame_;
-    AVFrame* sw_frame_;
-    SwsContext* sws_ctx_;
-    AVBufferRef* hw_device_ctx_;
-    AVPixelFormat hw_pix_fmt_;
-    bool use_hw_decode_;
-    bool end_of_stream_;
-
+    cv::VideoCapture cap_;
     std::string video_path_;
     int video_id_;
     int frame_number_;
